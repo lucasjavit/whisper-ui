d76939fc9f3c9e7343200b17a06d15ee814f30b2
lucasaguiar89 <lucasaguiar@eworldes.com>
2025-08-25 21:47:18 -0300

    commit front

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
 
-
-
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
